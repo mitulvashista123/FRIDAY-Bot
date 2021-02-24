664bafeabc83c84c2db134a5bc1ac0cba12a17a9
Mitul Vashista <mitulvashista123@gmail.com>
2021-02-25 04:01:41 +0530

    initial setup

---
+++ bot.py
@@ -0,0 +1,7 @@
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='./')
+
+bot.load_extension("cogs.maincog")
+
+bot.run("ODA2Nzk2MTMyOTE4NjI0MjY3.YBupcw.i9SAbTUI6kFc3Rit5mp5eqOLJqQ")
